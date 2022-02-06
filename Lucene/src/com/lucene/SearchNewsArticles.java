22df0d407d35b5517eb7d27ba74463cd5d336f13
Gaurav Gadewar <gauravgadewar35@hotmail.com>
2022-02-05 21:30:59 -0800

    Added Code for Lucene

---
+++ Lucene/src/com/lucene/SearchNewsArticles.java
@@ -0,0 +1,32 @@
+package com.lucene;
+
+import java.io.IOException;
+
+import org.apache.lucene.queryparser.classic.ParseException;
+
+public class SearchNewsArticles {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		
+//		String jsonFilesPath = "C:\\IRProject\\JSONFiles\\";
+//		String indexPath = "C:\\IRProject\\Index";
+//		String queryResultStorePath = "C:\\IRProject\\IndexData\\";
+//		String query = "covid vaccine";
+//		int numberOfArticlesToFetch = 5;
+//		
+//		LuceneIndexWriter index = new LuceneIndexWriter(jsonFilesPath, indexPath);
+//		index.createIndex();
+//
+//		LuceneIndexReader reader = new LuceneIndexReader(queryResultStorePath, indexPath);
+//		reader.search(query,numberOfArticlesToFetch);
+		
+		LuceneIndexWriter index = new LuceneIndexWriter(args[0], args[1]);
+		index.createIndex();
+
+		LuceneIndexReader reader = new LuceneIndexReader(args[2], args[1]);
+		int numberOfTopArticles = Integer.parseInt(args[4]);
+		reader.search(args[3],numberOfTopArticles);
+		
+	}
+
+}
