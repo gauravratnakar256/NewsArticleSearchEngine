593d7a59a0e618e27383bb6c32e04f2394453b54
adroitx <abhi.sharma1177@gmail.com>
2022-02-23 18:39:08 -0800

    UI

---
+++ webapp-SE/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  public searchForm: any;
+  private searchTypeLucene: boolean = true;
+  public loading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+    this.searchForm = this.formBuilder.group({
+      searchInput: ['', Validators.required],
+      searchType: ['lucene', Validators.required]
+    });
+  }
+  title = 'News Article Search Engine';
+
+  changeSearchType(event: any) {
+    this.searchTypeLucene = event.target.value === 'lucene' ? true : false;
+    if (this.searchTypeLucene){
+      console.log(`Search type Lucene is selected`);
+    } else {
+      console.log(`Search type Hadoop is selected`);
+    }
+  }
+
+  onSubmit() {
+    if (this.searchForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    this.loading = false;
+  }
+
+  get searchInput() {
+    return this.searchForm.get('searchInput');
+  }
+
+  get searchType() {
+    return this.searchForm.get('searchType');
+  }
+}
