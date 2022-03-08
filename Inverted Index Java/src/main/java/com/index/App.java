e37fcb94a22304004cce2d01a283c40b6d202895
Gaurav Gadewar <gauravgadewar35@hotmail.com>
2022-03-07 20:33:12 -0800

    Added java mapreduce Inverted Index

---
+++ Inverted Index Java/src/main/java/com/index/App.java
@@ -0,0 +1,22 @@
+package com.index;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String args[]) throws Exception {
+
+        File dir = new File(args[1]);
+        FileUtils.deleteDirectory(dir);
+
+        int exitCode = ToolRunner.run(new Driver(), args);
+
+
+    }
+}
